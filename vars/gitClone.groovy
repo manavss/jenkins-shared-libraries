d07ffe87b22b2908f2ec1900aeb7b5fe68429869
Manav Shete <shetemanav38@gmail.com>
2026-07-28 22:18:36 +0530

    Rename gitClone function to call

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -1,4 +1,4 @@
-def gitClone(String url,String branch){
+def call(String url,String branch){
                 echo "This is cloning the code"
                 git url: "${url}", branch: "${branch}"
                 echo "Code Cloned Successfully"
